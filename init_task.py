bcd252d6760cff768f6fa4631c56a1f9bb7a8322
Ewa Sawicka <ewajoanna076@gmail.com>
2023-02-22 19:47:38 +0100

    Change1

--- init_task.py
+++ init_task.py
@@ -1,6 +1,6 @@
 shop_dictionary = {
     "piekarnia":["chleb","bułki","pączek"],
-    "warzywniak":["marchew","seler","rukola"]
+    "warzywniak":["marchewki","seler","rukola"]
     }
 for key,value in shop_dictionary.items():
     value_copy = [word.title() for word in value] 
